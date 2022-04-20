a94070b3a23982460bf299de8ea6c633207f7116
ing181 <ingcei0123@edu.lerum.se>
2022-04-20 11:09:36 +0200

    lysdiod och ljuskänsligt motstånd

---
+++ light_meter/light_meter.ino
@@ -0,0 +1,22 @@
+
+int sensor;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(8,OUTPUT);
+  pinMode(A0,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor = analogRead(A0);
+  Serial.println(sensor);
+  
+    if (sensor < 600) {
+      digitalWrite(8,HIGH);
+    }
+    else {
+      digitalWrite(8,LOW);
+    }
+}
